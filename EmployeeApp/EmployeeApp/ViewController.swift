e34770050e587dd8c30000d9c848e4f0840c4a97
Briahna Santillana <santilla18@up.edu>
2017-10-05 19:27:16 -0700

    Employee App UI

---
+++ EmployeeApp/EmployeeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmployeeApp
+//
+//  Created by Briahna Santillana on 10/5/17.
+//  Copyright © 2017 Briahna Santillana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ EmployeeApp/OrderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OrderTableViewCell.swift
+//  EmployeeApp
+//
+//  Created by Briahna Santillana on 10/5/17.
+//  Copyright © 2017 Briahna Santillana. All rights reserved.
+//
+
+import UIKit
+
+class OrderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
